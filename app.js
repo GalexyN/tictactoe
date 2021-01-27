a043fe64af970b9369dc91c099f7a202387c2088
galexy n <alexneeeeewin@icloud.com>
2021-01-26 19:10:10 -0800

    created working buttons to register moves on the board

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const buttons = [...window.document.getElementsByTagName('button')];
+// adds event listener to each button to register moves
+setTimeout(() => {
+  const buttons = [...document.getElementsByTagName('button')];
+  
+  buttons.forEach(button => button.addEventListener('click', () => {
+    button.textContent = 'clicked'
+    console.log(`Move registered in ${button.parentElement.id}`)
+  }));
 
-buttons.forEach(button => button.addEventListener('click', () => console.log('click')))
+}, 0);
 
+// create the 2 players 
+
+// implement win conditions checks
